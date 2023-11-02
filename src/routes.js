d49351282500919ed026751b448d95d76e6d2374
Victor Dias <lzero1541@gmail.com>
2023-11-02 16:25:40 -0300

    implement creating appointment

--- src/routes.js
+++ src/routes.js
@@ -16,4 +16,26 @@ router.get("/appointments", async (request, response) => {
   return response.json(appointments);
 });
 
+router.post("/appointments", async (request, response) => {
+  const appointmentsService = new AppointmentsService();
+
+  const appointment = request.body;
+
+  if (!appointment || !appointment.description || !appointment.date) {
+    return response.status(400).json({
+      message: "required is falied because missing data or description",
+    });
+  }
+
+  const newAppointment = await appointmentsService.createAppointment(
+    appointment
+  );
+
+  if (newAppointment) {
+    return response.json(newAppointment);
+  }
+
+  return response.status(500).json({ message: "internal error" });
+});
+
 export { router };
--- src/services/AppointmentsService.js
+++ src/services/AppointmentsService.js
@@ -1,11 +1,11 @@
 const dataBase = [
   {
     description: "Consulta com o Doutor Ricardo",
-    date: new Date("10/20/2010"),
+    date: new Date("2023-11-02T12:27:38.684Z"),
   },
   {
     description: "Consulta com o Doutor Douglas",
-    date: new Date("08/12/2012"),
+    date: new Date("2023-11-03T12:27:38.684Z"),
   },
 ];
 
@@ -16,5 +16,25 @@ export class AppointmentsService {
       return [];
     }
     return appointments;
+  }
+
+  async createAppointment(appointment) {
+    const tryFindDate = dataBase.find((ap) => {
+      return (
+        new Date(ap.date).getTime() === new Date(appointment.date).getTime()
+      );
+    });
+
+    if (tryFindDate) {
+      return { message: "alredy exists a equal date" };
+    }
+
+    const newAppointment = {
+      description: appointment.description,
+      date: appointment.date,
+    };
+
+    dataBase.push(newAppointment);
+    return newAppointment;
   }
 }
